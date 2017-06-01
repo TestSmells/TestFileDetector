8cfab7a23fb88064eb2570f362f89587c4318b54
shehan peruma <shehan.peruma@gmail.com>
2017-06-01 16:42:11 -0400

    updated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,16 +9,28 @@ public class Main {
         String file = "C:\\Projects\\TestSmells_ExisitngTools\\samples\\org.xbmc.kore\\app\\src\\test\\java\\org\\xbmc\\kore\\provider\\mediaprovider\\TestValues.java";
         TestFileDetector testFileDetector;
 
-
+        //recursively identify all 'java' files in the specified directory
+        System.out.println("Started - Identify all 'java' files");
         FileWalker fw = new FileWalker();
-        List<FileEntity> files = fw.getJavaTestFiles("C:\\Projects\\TestSmells_ExisitngTools\\samples",true);
+        List<FileEntity> files = fw.getJavaFiles("C:\\Projects\\TestSmells_ExisitngTools\\samples",true);
+        System.out.println("Ended - Identify all 'java' files");
 
 
+        //foreach of the identified 'java' files, obtain details about the methods that they contain
+        System.out.println("Started - Obtain method details");
         for (FileEntity fileEntity: files) {
             testFileDetector = new TestFileDetector();
             fileEntity.setMethods(testFileDetector.parseFile(fileEntity.getFilePath()));
         }
+        System.out.println("Ended - Obtain method details");
 
-        System.out.println(files.size());
+
+        //output the results into CSV files
+        System.out.println("Started - Save results to CSV");
+        ResultsWriter resultsWriter = new ResultsWriter(files);
+        resultsWriter.outputToCSV(true);
+        System.out.println("Ended - Save results to CSV");
+
+
     }
 }
