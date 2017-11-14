855a9c3c281744ef5c020882454c8e6aa5088cd9
2018SANER <2018SANER@Gmail.com>
2017-11-14 17:29:44 -0500

    updated logic to check for 'test' in file name

--- src/main/java/entity/FileEntity.java
+++ src/main/java/entity/FileEntity.java
@@ -1,4 +1,4 @@
-package  entity;
+package entity;
 
 import java.nio.file.Path;
 import java.util.List;
@@ -36,7 +36,7 @@ public class FileEntity {
     }
 
     public long getTestMethodWithoutAnnotationCount() {
-        return methods.parallelStream().filter((p)->p.isHasAnnotation()==false && p.isHasTestInName()==true).count();
+        return methods.parallelStream().filter((p) -> p.isHasAnnotation() == false && p.isHasTestInName() == true).count();
     }
 
     public String getFilePath() {
@@ -44,67 +44,74 @@ public class FileEntity {
     }
 
     public long getTestAnnotationCount() {
-        return methods.parallelStream().filter((p)->p.isHasAnnotation()).count();
+        return methods.parallelStream().filter((p) -> p.isHasAnnotation()).count();
     }
 
-    public boolean isHasTestInFileName(){
-        return  path.getFileName().toString().toLowerCase().contains("test");
+    public boolean isHasTestInFileName() {
+        String fileName = getFileNameWithoutExtension().toLowerCase();
+        if (fileName.startsWith("test") || fileName.startsWith("tests") || fileName.endsWith("test") || fileName.endsWith("tests"))
+            return true;
+        else
+            return false;
     }
 
-    public String getFileName(){
+    public String getFileName() {
         return path.getFileName().toString();
+    }
+
+    public String getFileNameWithoutExtension() {
+        String fileName = path.getFileName().toString().substring(0,path.getFileName().toString().toLowerCase().lastIndexOf(".java"));
+        return fileName;
     }
 
     public long getTotalTestMethods() {
         return (getTestMethodWithoutAnnotationCount() + getTestAnnotationCount());
     }
 
-    public boolean getHas_junitframeworkTest(){
+    public boolean getHas_junitframeworkTest() {
         return imports.get("junit.framework.Test");
     }
 
-    public boolean getHas_junitframeworkTestCase(){
+    public boolean getHas_junitframeworkTestCase() {
         return imports.get("junit.framework.TestCase");
     }
 
-    public boolean getHas_orgjunitTest(){
+    public boolean getHas_orgjunitTest() {
         return imports.get("org.junit.Test");
     }
 
-    public boolean getHas_androidtestAndroidTestCase(){
+    public boolean getHas_androidtestAndroidTestCase() {
         return imports.get("android.test.AndroidTestCase");
     }
 
-    public boolean getHas_androidtestInstrumentationTestCase()
-    {
-        return  imports.get("android.test.InstrumentationTestCase");
+    public boolean getHas_androidtestInstrumentationTestCase() {
+        return imports.get("android.test.InstrumentationTestCase");
     }
 
-    public boolean getHas_androidtestActivityInstrumentationTestCase2()
-    {
-        return  imports.get("android.test.ActivityInstrumentationTestCase2");
+    public boolean getHas_androidtestActivityInstrumentationTestCase2() {
+        return imports.get("android.test.ActivityInstrumentationTestCase2");
     }
 
-    public boolean getHas_orgjunitAssert(){
+    public boolean getHas_orgjunitAssert() {
         return imports.get("org.junit.Assert");
     }
 
-    public String getRelativeFilePath(){
+    public String getRelativeFilePath() {
         String filePath = path.toAbsolutePath().toString();
         String[] splitString = filePath.split("\\\\");
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < 5; i++) {
             stringBuilder.append(splitString[i] + "\\");
         }
-        return filePath.substring(stringBuilder.toString().length()).replace("\\","/");
+        return filePath.substring(stringBuilder.toString().length()).replace("\\", "/");
     }
 
-    public String getAppName(){
+    public String getAppName() {
         String filePath = path.toAbsolutePath().toString();
         return filePath.split("\\\\")[3];
     }
 
-    public String getTagName(){
+    public String getTagName() {
         String filePath = path.toAbsolutePath().toString();
         return filePath.split("\\\\")[4];
     }
