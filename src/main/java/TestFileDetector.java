c26f941adc02d1e5eeecc84dab488c528b8ad9f5
shehan <shehan.peruma@gmail.com>
2018-02-13 18:26:33 -0500

    code to detect comments that have technical debt specific keyword(s)

--- src/main/java/TestFileDetector.java
+++ src/main/java/TestFileDetector.java
@@ -3,6 +3,7 @@ import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.ImportDeclaration;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.comments.Comment;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 import entity.ClassEntity;
 import entity.MethodEntity;
@@ -12,6 +13,8 @@ import java.io.FileNotFoundException;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class TestFileDetector {
@@ -46,6 +49,7 @@ public class TestFileDetector {
         CompilationUnit compilationUnit=null;
 
         if(filePath != null){
+            System.out.println("Processing: "+filePath);
             classEntity = new ClassEntity(filePath);
             FileInputStream fileInputStream = new FileInputStream(classEntity.getFilePath());
             compilationUnit = JavaParser.parse(fileInputStream);
@@ -61,11 +65,62 @@ public class TestFileDetector {
 
     private class ClassVisitor extends VoidVisitorAdapter<Void> {
 
+        //keywords obtained from: http://ieeexplore.ieee.org/document/7820211/
+        private final String[] DEBT_KEYWORDS = {"hack", "workaround", "yuck!", "kludge", "stupidity","needed?","unused?","fixme","todo","wtf?"};
+
         @Override
         public void visit(ClassOrInterfaceDeclaration n, Void arg) {
             classEntity.setClassName(n.getNameAsString());
+
+            for (Comment comment:n.getAllContainedComments()) {
+                if (containsKeyword(comment.getContent())){
+                    System.out.println(comment.getContent());
+                }
+            }
+
+            if(n.getComment().isPresent()){
+                if (containsKeyword(n.getComment().get().getContent())){
+                    System.out.println(n.getComment().get().getContent());
+                }
+            }
+
+            if(n.getJavadocComment().isPresent()){
+                if (containsKeyword(n.getJavadocComment().get().getContent())){
+                    System.out.println(n.getJavadocComment().get().getContent());
+                }
+            }
+
+            for (Comment comment:n.getOrphanComments()) {
+                if (containsKeyword(comment.getContent())){
+                    System.out.println(comment.getContent());
+                }
+            }
+
+
             super.visit(n, arg);
         }
+
+        private Boolean containsKeyword(String input){
+            Boolean hasKeyword = false;
+
+            Pattern pattern;
+            Matcher matcher;
+            for (String keyword : DEBT_KEYWORDS) {
+                pattern = Pattern.compile("\\b"+keyword+"\\b",Pattern.CASE_INSENSITIVE);
+                matcher = pattern.matcher(input);
+                while (matcher.find()) {
+                /*
+                    System.out.print("Start index: " + matcher.start());
+                    System.out.print(" End index: " + matcher.end() + " ");
+                    System.out.println(matcher.group());
+                */
+                    return true;
+                }
+            }
+
+            return hasKeyword;
+        }
+
 
         @Override
         public void visit(MethodDeclaration n, Void arg) {
