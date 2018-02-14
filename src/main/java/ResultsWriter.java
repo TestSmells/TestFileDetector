59347eba14cf3710bd4dd097b8bf66de3b9ec1e6
shehan <shehan.peruma@gmail.com>
2018-02-14 03:03:14 +0000

    save technical debt comments in a csv file

--- src/main/java/ResultsWriter.java
+++ src/main/java/ResultsWriter.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 public class ResultsWriter {
 
-    private CSVWriter classCSVWriter, methodCSVWriter;
+    private CSVWriter classCSVWriter, methodCSVWriter, debtCSVWriter;
 
     public static ResultsWriter createResultsWriter() throws IOException {
         return new ResultsWriter();
@@ -21,11 +21,30 @@ public class ResultsWriter {
         String time = String.valueOf(Calendar.getInstance().getTimeInMillis());
         String classFileName = MessageFormat.format("{0}_{1}_{2}.{3}", "Output", "Class", time, "csv");
         String methodFileName = MessageFormat.format("{0}_{1}_{2}.{3}", "Output", "Method", time, "csv");
+        String debtFileName = MessageFormat.format("{0}_{1}_{2}.{3}", "Output", "Debt", time, "csv");
         methodCSVWriter = new CSVWriter(new FileWriter(methodFileName), ',');
         classCSVWriter = new CSVWriter(new FileWriter(classFileName), ',');
+        debtCSVWriter = new CSVWriter(new FileWriter(debtFileName), ',');
 
         createClassFile();
         createMethodFile();
+        createDebtFile();
+    }
+    private void createDebtFile() throws IOException {
+        List<String[]> fileLines = new ArrayList<String[]>();
+        String[] columnNames = {
+                "App",
+                "Tag",
+                "FilePath",
+                "RelativeFilePath",
+                "FileName",
+                "ClassName",
+                "Comment"
+        };
+        fileLines.add(columnNames);
+
+        debtCSVWriter.writeAll(fileLines, false);
+        debtCSVWriter.flush();
     }
 
     private void createClassFile() throws IOException {
@@ -88,11 +107,33 @@ public class ResultsWriter {
     public void outputToCSV(ClassEntity classEntity) throws IOException {
         outputClassDetails(classEntity);
         outputMethodDetails(classEntity);
+        outputDebtDetails(classEntity);
+    }
+
+    private void outputDebtDetails(ClassEntity classEntity) throws IOException {
+        List<String[]> fileLines = new ArrayList<String[]>();
+        String[] dataLine;
+        for (String comment : classEntity.getTechnicalDebtComments()) {
+
+            dataLine = new String[7];
+            dataLine[0] = classEntity.getAppName();
+            dataLine[1] = classEntity.getTagName();
+            dataLine[2] = classEntity.getFilePath();
+            dataLine[3] = classEntity.getRelativeFilePath();
+            dataLine[4] = classEntity.getFileName();
+            dataLine[5] = classEntity.getClassName();
+            dataLine[6] = "\""+comment.replace('"',' ')+"\"";
+
+            fileLines.add(dataLine);
+        }
+        debtCSVWriter.writeAll(fileLines, false);
+        debtCSVWriter.flush();
     }
 
     public void closeOutputFiles() throws IOException {
         classCSVWriter.close();
         methodCSVWriter.close();
+        debtCSVWriter.close();
     }
 
     private void outputMethodDetails(ClassEntity classEntity) throws IOException {
